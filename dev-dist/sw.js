d741b7e11c5f09bf5be0d9607e792281eedb6b63
bntkpp <thewild81@gmail.com>
2026-05-20 20:24:45 -0400

    Actualiza la revisión del archivo index.html en el service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.aq1nnach5a"
+    "revision": "0.f194dkm9ob8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
